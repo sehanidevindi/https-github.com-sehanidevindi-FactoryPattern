5b400bb616260a671b3c6354db9312124bc8c42d
Sehani Devindi <sehanidevindi@gmail.com>
2021-01-07 20:56:44 +0530

    Add project files.

---
+++ FactoryPattern/com._03Factory/Divide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.com._03Factory
+{
+    class Divide
+    {
+        public void Calculate(double a, double b)
+        {
+            Console.WriteLine("a/b is {0}",a / b);
+
+        
+        }
+    }
+}
---
+++ FactoryPattern/com._03Factory/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.com._03Factory
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number");
+            string input = Console.ReadLine();
+            double num1, num2;
+            bool result = Double.TryParse(input,out num1);
+            if (!result)
+            {
+                Console.WriteLine("please enter a number");
+                return;
+            }
+            Console.WriteLine("Enter second number");
+            result = Double.TryParse(Console.ReadLine(), out num2);
+            if (!result)
+            {
+                Console.WriteLine("Please enter a number");
+                return;
+            }
+
+            Divide obj = new Divide();
+            obj.Calculate(num1,num2);
+        }
+    }
+}
